54baf1cf37a8189fae909f7242d7314724d8f47d
gucheen <guchengf@gmail.com>
2026-09-03 18:43:57 +0800

    Add listing abbr

--- commands.d/30-abbreviations.sh
+++ commands.d/30-abbreviations.sh
@@ -1,3 +1,6 @@
 # @cmd Network | mtr100 <host> | Expand to a 100-cycle IPv4 MTR command
 abbr --define mtr100 'mtr -rwzc 100 -4'
-
+abbr --define l    eza
+abbr --define ll   'eza -l'
+abbr --define la   'eza -la'
+abbr --define ldir 'eza -ld'
